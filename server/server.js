9b1bb14f6653d9773cc62b49e89b6831c8a99118
Your Name <you@example.com>
2019-01-02 21:31:39 -0800

    First commit. Three components running, CORS issue still present

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const morgan = require('morgan');
 const app = express();
+const cors = require('cors');
 
-app.use('/restaurant/photos', express.static(`${dirname}/client/dist/`));
+app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+app.use(bodyParser());
+app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const port = 3003;
 app.listen(port, () => {
   console.log(`Listening on port ${port} for FEC project`);
-})
+});
