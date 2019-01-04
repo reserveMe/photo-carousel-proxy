29bd20838fd72ad52a9b22fe42f6ddac757f1991
Your Name <you@example.com>
2019-01-03 20:00:36 -0800

    All four components rendering. CSS not applied, most likely requires stylesheet import

--- server/server.js
+++ server/server.js
@@ -1,26 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const app = express();
 const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
 
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-
-app.use(cors({ origin: 'http://localhost:3000' }));
+// app.use(cors({ origin: 'http://localhost:3000' }));
 
 app.use(bodyParser());
-app.use('/restaurants/:id/', express.static(`${__dirname}/../client/dist/`));
+app.use('/restaurants/:id/', express.static(`${__dirname}/../public/`));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+const port = 3000;
 
-const port = 3003;
 app.listen(port, () => {
-  console.log(`Listening on port ${port} for FEC project`);
+  console.log(`server running at: http://localhost:${port}`);
 });
